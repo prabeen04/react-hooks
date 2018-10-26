6fa83770e0fe5be38727aeac0212d352b58923a4
Prabeen04 <prabeen.strange@gmail.com>
2018-10-26 20:56:36 +0530

    imports and theme styles added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import CounterHook from './components/CounterHook'
 import Navbar from './components/Navigation/Navbar'
+import './App.css';
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
         <Navbar />
         <CounterHook />
-      </div>
+      </React.Fragment>
     );
   }
 }
--- src/components/CounterHook.js
+++ src/components/CounterHook.js
@@ -1,5 +1,18 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 
+const styles = theme => ({
+    button: {
+      margin: theme.spacing.unit,
+    },
+    extendedIcon: {
+      marginRight: theme.spacing.unit,
+    },
+  });
+  
 export default function CounterHook() {
     const [count, setCount] = useState(0)
     return (
