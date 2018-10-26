874d00f6da23f517cea7c307ad43f2cbecd56b14
Prabeen04 <prabeen.strange@gmail.com>
2018-10-26 21:00:01 +0530

    styles and layout adjusted

--- src/components/CounterHook.js
+++ src/components/CounterHook.js
@@ -6,22 +6,27 @@ import AddIcon from '@material-ui/icons/Add';
 
 const styles = theme => ({
     button: {
-      margin: theme.spacing.unit,
+        margin: theme.spacing.unit,
     },
     extendedIcon: {
-      marginRight: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
     },
-  });
-  
-export default function CounterHook() {
+});
+
+const CounterHook = (props) => {
+    const { classes } = props;
     const [count, setCount] = useState(0)
     return (
         <div>
             <h3>{count}</h3>
-            <button
-                onClick={() => setCount(count + 4)}>
-                Increase
-            </button>
+            <div style={{ position: 'fixed', bottom: 10, right: 10 }}>
+                <Button variant="fab" color="primary" aria-label="Add" className={classes.button}
+                    onClick={() => setCount(count + 4)}>
+                    <AddIcon />
+                </Button>
+            </div>
         </div>
     )
 }
+
+export default withStyles(styles)(CounterHook);
