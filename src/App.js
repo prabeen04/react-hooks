8649acbf18587f0b95b2dcbf8232df3da9724e1a
Prabeen04 <prabeen.strange@gmail.com>
2018-10-26 20:44:15 +0530

    material-ui added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import CounterHook from './components/CounterHook'
 class App extends Component {
   render() {
     return (
       <div>
+        <CounterHook/>
       </div>
     );
   }
--- src/components/CounterHook.js
+++ src/components/CounterHook.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 
 export default function CounterHook() {
-    const [count,setCount] = useState(0)
-  return (
-    <div>
-      {count}
-    </div>
-  )
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+            <h3>{count}</h3>
+            <button
+                onClick={() => setCount(count + 4)}>
+                Increase
+            </button>
+        </div>
+    )
 }
