466d10dd416b61182038cda656f5f99d248f7aa7
Prabeen04 <prabeen.strange@gmail.com>
2018-10-27 17:54:16 +0530

    useAdvice custom hook created

--- src/CustomHooks/AdviceHook.js
+++ src/CustomHooks/AdviceHook.js
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
 
-export default function getAdvice(url) {
-    fetch(`http://api.adviceslip.com/advice`)
-        .then(res => res.json())
-        .then(res => console.log(res))
-        .catch(err => console.log(err)
+export default function useAdvice(url) {
+    const [advice, setAdvice] = useState(null);
+    useEffect(() => {
+        fetch(`http://api.adviceslip.com/advice`)
+            .then(res => res.json())
+            .then(res => {
+                setAdvice(res)
+                console.log(res)
+            })
+            .catch(err => console.log(err)
+        })
+    return advice
 }
