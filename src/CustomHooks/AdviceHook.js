8503c412a3f802aada535773cc8ea7728aee6d74
Prabeen04 <prabeen.strange@gmail.com>
2018-10-27 19:33:17 +0530

    setTimeout added

--- src/CustomHooks/AdviceHook.js
+++ src/CustomHooks/AdviceHook.js
@@ -3,13 +3,16 @@ import { useState, useEffect } from "react";
 export default function useAdvice(url) {
     const [advice, setAdvice] = useState(null);
     useEffect(() => {
-        fetch(`http://api.adviceslip.com/advice`)
-            .then(res => res.json())
-            .then(res => {
-                setAdvice(res)
-                console.log(res)
-            })
-            .catch(err => console.log(err))
+        setTimeout(() =>{
+            fetch(`http://api.adviceslip.com/advice`)
+                .then(res => res.json())
+                .then(res => {
+                    setAdvice(res)
+                    console.log(res)
+                })
+                .catch(err => console.log(err))
+        }, 3000)
+        return () => clearTimeout();
         })
     return advice
 }
--- src/components/CounterHook.js
+++ src/components/CounterHook.js
@@ -18,18 +18,22 @@ const CounterHook = (props) => {
     const { classes } = props;
     const [count, setCount] = useState(0)
     const advice = useAdvice('abc')
-    console.log(advice)
+    // console.log(advice)
     document.title = `Current Count: ${count}`;
     return (
         <div>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Typography component="h2" variant="display1" gutterBottom>
-                   {/* { advice && <pre>{advice.slip.advice}</pre>} */}
+                   { advice && <pre>{advice.slip.advice}</pre>}
                 </Typography>
             </div>
             <div style={{ position: 'fixed', bottom: 10, right: 10 }}>
                 <Button variant="fab" color="primary" aria-label="Add" className={classes.button}
-                    onClick={() => console.log('button clicked')}>
+                    onClick={() => {
+                        console.log('button clicked')
+                        // const advice = useAdvice('abd');
+                        // console.log(advice)
+                        }}>
                     <AddIcon />
                 </Button>
             </div>
