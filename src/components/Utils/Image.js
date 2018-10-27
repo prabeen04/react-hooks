c0b4801f192664258db7b54a74fbabb8c5e0a9b0
Prabeen04 <prabeen.strange@gmail.com>
2018-10-27 21:24:49 +0530

    the-platform Img util implemented

--- src/components/Utils/Image.js
+++ src/components/Utils/Image.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { Img } from "the-platform";
 export default function Image() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <React.Suspense maxDuration={300} fallback={'loading...'}>
+                <Img src="https://source.unsplash.com/random/4000x2000" />
+            </React.Suspense>
+        </div>
+    )
 }
