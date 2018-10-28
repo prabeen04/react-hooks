326b790794c4d43600bc41f16b56c45acd9cad80
Prabeen04 <prabeen.strange@gmail.com>
2018-10-28 16:05:57 +0530

    card content added

--- src/components/Pages/Unsplash.js
+++ src/components/Pages/Unsplash.js
@@ -23,7 +23,33 @@ const styles = {
 export default function Unsplash() {
   return (
     <div>
-      <Image/>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Lizard
+          </Typography>
+            <Typography component="p">
+              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+              across all continents except Antarctica
+          </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+        </Button>
+          <Button size="small" color="primary">
+            Learn More
+        </Button>
+        </CardActions>
+      </Card>
+      <Image />
     </div>
   )
 }
