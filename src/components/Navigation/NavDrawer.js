d9f4f7282ff32ce2b40aaaa73e96376bbaa869d7
Prabeen04 <prabeen.strange@gmail.com>
2018-10-30 20:37:14 +0530

    navbar menu removed

--- src/components/Navigation/NavDrawer.js
+++ src/components/Navigation/NavDrawer.js
@@ -27,16 +27,7 @@ class NavDrawer extends React.Component {
         const sideList = (
             <div className={classes.list}>
                 <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-                <Divider />
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    {['Home', 'News'].map((text, index) => (
                         <ListItem button key={text}>
                             <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
                             <ListItemText primary={text} />
