1bcd3857b493395523660d63c4bdcb87c7293fd6
Prabeen04 <prabeen.strange@gmail.com>
2018-10-30 21:22:42 +0530

    small stuff

--- src/components/Pages/News/NewsList.js
+++ src/components/Pages/News/NewsList.js
@@ -1,11 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -21,6 +17,7 @@ const styles = theme => ({
         minWidth: 140,
     },
 });
+
 function NewsList(props) {
     const { classes } = props;
 
