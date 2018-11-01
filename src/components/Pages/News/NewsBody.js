29fb40f1305716d11a30832e25781fa402a1680e
Prabeen04 <prabeen.strange@gmail.com>
2018-11-01 21:12:41 +0530

    setFetching state created

--- src/components/Pages/News/NewsBody.js
+++ src/components/Pages/News/NewsBody.js
@@ -1,21 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { news_api_key, news_api_url } from "../../../config";
 const NewsBody = (props) => {
-    const [articles, setArticles] = useState(null)
+    const [articles, setArticles] = useState(null);
+    const [fetching, setFetching] = useState(true)
     useEffect(() => {
+        setFetching(true)
         fetch(`${news_api_url}articles?source=${props.selectedSource}&sortBy=top&apiKey=${news_api_key}`)
             .then(res => res.json())
             .then(res => {
+                setFetching(false)
                 console.log(res)
                 setArticles(res.articles)
             })
-            .catch(err => console.log(err))
+            .catch(err => {
+                setFetching(false)
+                console.log(err)
+            })
     })
     return (
         <div>
-            {!props.article && <p>Loading...</p>}
+            {fetching && <p>Loading...</p>}
             {articles && articles.map((article, i) => {
-               return <h3 key={i}>{article.title}</h3>
+                return <h3 key={i}>{article.title}</h3>
             })}
         </div>
     )
