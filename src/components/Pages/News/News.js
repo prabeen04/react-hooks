8b3738c481a750d729a493328e1598592aac028c
Prabeen04 <prabeen.strange@gmail.com>
2018-11-01 20:01:51 +0530

    state moved upward to news

--- src/components/Pages/News/News.js
+++ src/components/Pages/News/News.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import NewsHeader from './NewsHeader';
+import NewsBody from './NewsBody';
+import { news_api_url } from '../../../config';
+
 function News() {
+  const [sources, setSources] = useState([]);
+  useEffect(() => {
+    fetch(`${news_api_url}/sources`)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        setSources(res.sources)
+      })
+      .catch(err => console.log(err))
+  }, [])
   return (
-    <div>
-      <NewsHeader/>
-    </div>
+    <React.Fragment>
+      <NewsHeader />
+      <NewsBody />
+    </React.Fragment>
   )
 }
 
