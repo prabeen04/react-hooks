7ed81f6880bab5ad9b1b8c32ab83868dba0e8559
Prabeen04 <prabeen.strange@gmail.com>
2018-11-01 21:21:52 +0530

    theme styles created

--- src/components/Pages/News/LoadingNews.js
+++ src/components/Pages/News/LoadingNews.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    colorPrimary: {
+        backgroundColor: '#B2DFDB',
+    },
+    barColorPrimary: {
+        backgroundColor: '#00695C',
+    },
+};
+const LoadingNews = (props) => {
+    return (
+        <div>
 
- const LoadingNews = (props) => {
-  return (
-    <div>
-      
-    </div>
-  )
+        </div>
+    )
 }
-export default LoadingNews
+export default withStyles(styles)(LoadingNews)
