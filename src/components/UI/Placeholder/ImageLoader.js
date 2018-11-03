ee48676457015938c8c07e1569ee26de484c5b8e
Prabeen04 <prabeen.strange@gmail.com>
2018-11-03 20:29:52 +0530

    CircularProgress added in imageLoader

--- src/components/UI/Placeholder/ImageLoader.js
+++ src/components/UI/Placeholder/ImageLoader.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-class ImageLoader extends Component {
-  render() {
+const styles = theme => ({
+    progress: {
+        margin: theme.spacing.unit * 2,
+    },
+});
+
+const ImageLoader = (props) => {
+    const { classes } = props;
     return (
-      <div>
-        
-      </div>
-    )
-  }
+        <div>
+            <CircularProgress className={classes.progress} size={50} />
+        </div>
+    );
+}
 }
 
-export default ImageLoader;
+export default withStyles(styles)(ImageLoader);
