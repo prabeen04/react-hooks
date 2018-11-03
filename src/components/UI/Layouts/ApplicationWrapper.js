5b58a70eee1c9268c87d19343762d988ba8bdeed
Prabeen04 <prabeen.strange@gmail.com>
2018-11-03 20:15:36 +0530

    application style stuff

--- src/components/UI/Layouts/ApplicationWrapper.js
+++ src/components/UI/Layouts/ApplicationWrapper.js
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 
 const ApplicationWrapper = styled.div`
-    width: 100vw;
-    height: 100%;
+    min-height: 100vh;
     overflow: hidden;
     background-color: #eeeeee;
 `
