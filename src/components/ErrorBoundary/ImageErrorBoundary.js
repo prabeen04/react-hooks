da519cbc6eecfa73f2309244c09af728a1b8cbbe
Prabeen04 <prabeen.strange@gmail.com>
2018-11-05 23:37:22 +0530

    ImageErrorBoundary component created

---
+++ src/components/ErrorBoundary/ImageErrorBoundary.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ImageErrorBoundary extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ImageErrorBoundary;
