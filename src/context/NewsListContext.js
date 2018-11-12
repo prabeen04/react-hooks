6a625954a3fbe7e5c767534ab07fb4df242c98ab
Prabeen04 <prabeen.strange@gmail.com>
2018-11-12 19:56:09 +0530

    NewsList context provider created

--- src/context/NewsListContext.js
+++ src/context/NewsListContext.js
@@ -1,3 +1,11 @@
 import React from 'react'
 
 const NewsListContext = React.createContext('dummy list');
+
+export default function NewsListContext() {
+  return (
+    <NewsListContext.Provider value={'dummy list'}>
+      <h2>News list context</h2>
+    </NewsListContext.Provider>
+  )
+}
