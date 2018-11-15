09d17d6153883c426125b312cdf7078dc7576c24
Prabeen04 <prabeen.strange@gmail.com>
2018-11-15 19:30:27 +0530

    TodoList lazyly added

--- src/components/Pages/Todo/Todo.js
+++ src/components/Pages/Todo/Todo.js
@@ -1,10 +1,13 @@
-import React,{ useState } from 'react'
+import React, { lazy, Suspense, useState } from 'react'
+
+const TodoList = lazy(() => import('./TodoList'));
 
 const Todo = () => {
-const [todo, setsTodo ] = useState('code')
+  const [todo, setsTodo] = useState('code')
   return (
     <>
       <h3>{todo}</h3>
+      <TodoList />
     </>
   )
 }
--- src/components/Pages/Todo/TodoList.js
+++ src/components/Pages/Todo/TodoList.js
@@ -3,7 +3,7 @@ import React from 'react'
 const TodoList = () => {
   return (
     <>
-      
+      <h2>Todo List</h2>
     </>
   )
 }
