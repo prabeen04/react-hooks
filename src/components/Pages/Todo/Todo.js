e344bbd950d1bc5135cd95950556551d6b020188
Prabeen04 <prabeen.strange@gmail.com>
2018-11-16 18:54:08 +0530

    handleTodomethod created

--- src/components/Pages/Todo/Todo.js
+++ src/components/Pages/Todo/Todo.js
@@ -3,10 +3,13 @@ import React, { lazy, Suspense, useState } from 'react'
 const TodoList = lazy(() => import('./TodoList'));
 
 const Todo = () => {
-  const [todos, setsTodos] = useState([{ todo: 'code', iscompleted: false },{ todo: 'code again', isCompleted: true }])
+  const [todos, setsTodos] = useState([{ todo: 'code', iscompleted: false }, { todo: 'code again', isCompleted: true }])
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+      return newTodos.splice(index, 1)
+  }
   return (
     <>
-      <h3>Todo's</h3>
       <TodoList todos={todos} />
     </>
   )
