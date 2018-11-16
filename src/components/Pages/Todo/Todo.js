69987a6c6a359dace5ab655e5abef7d5537308be
Prabeen04 <prabeen.strange@gmail.com>
2018-11-16 18:43:02 +0530

    list imports added

--- src/components/Pages/Todo/Todo.js
+++ src/components/Pages/Todo/Todo.js
@@ -3,11 +3,11 @@ import React, { lazy, Suspense, useState } from 'react'
 const TodoList = lazy(() => import('./TodoList'));
 
 const Todo = () => {
-  const [todos, setsTodos] = useState([{todo: 'code', iscompleted: false}])
+  const [todos, setsTodos] = useState([{ todo: 'code', iscompleted: false }])
   return (
     <>
       <h3>Todo's</h3>
-      <TodoList />
+      <TodoList todos={todos} />
     </>
   )
 }
--- src/components/Pages/Todo/TodoList.js
+++ src/components/Pages/Todo/TodoList.js
@@ -1,4 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import Avatar from '@material-ui/core/Avatar';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
 
 const TodoList = () => {
   return (
@@ -7,4 +23,7 @@ const TodoList = () => {
     </>
   )
 }
-export default TodoList;
+TodoList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+export default  withStyles(styles)(TodoList);
