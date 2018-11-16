1935ee38064683e8629ba7eba7cbacbf2cda5a06
Prabeen04 <prabeen.strange@gmail.com>
2018-11-16 18:51:10 +0530

    small stuff

--- src/components/Pages/Todo/TodoList.js
+++ src/components/Pages/Todo/TodoList.js
@@ -19,13 +19,12 @@ const styles = theme => ({
 const TodoList = ({todos, classes}) => {
   return (
     <>
-      <h2>Todo List</h2>
       <div className={classes.root}>
         <List dense>
           {todos.map(todo => (
             <ListItem key={todo} button>
               <Avatar alt="Remy Sharp" src="https://cdn.iconscout.com/icon/free/png-256/avatar-375-456327.png" />
-              <ListItemText primary={`Line item ${`todo + 1`}`} />
+              <ListItemText primary={` ${todo.todo}`} />
               <ListItemSecondaryAction>
                 <Checkbox
                   // onChange={this.handleToggle(`todo`)}
