499f455437ffcfb3fb40a506b98cc5c999795038
Prabeen04 <prabeen.strange@gmail.com>
2018-11-17 22:11:40 +0530

    material-ui imports and styles added

--- src/components/Pages/Todo/TodoForm.js
+++ src/components/Pages/Todo/TodoForm.js
@@ -1,5 +1,23 @@
 import React from 'react'
-
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+const styles = theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing.unit,
+      marginRight: theme.spacing.unit,
+    },
+    dense: {
+      marginTop: 16,
+    },
+    menu: {
+      width: 200,
+    },
+  });
 const TodoForm = () => {
     return (
         <div>
@@ -8,4 +26,4 @@ const TodoForm = () => {
     )
 }
 
-export default TodoForm;
+export default withStyles(styles)(TodoForm);
