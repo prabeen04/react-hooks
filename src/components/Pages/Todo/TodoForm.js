5286c3b5ed214e369711aee894365c38fb1d9299
Prabeen04 <prabeen.strange@gmail.com>
2018-11-17 22:12:54 +0530

    Material TextField added

--- src/components/Pages/Todo/TodoForm.js
+++ src/components/Pages/Todo/TodoForm.js
@@ -4,25 +4,34 @@ import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 const styles = theme => ({
     container: {
-      display: 'flex',
-      flexWrap: 'wrap',
+        display: 'flex',
+        flexWrap: 'wrap',
     },
     textField: {
-      marginLeft: theme.spacing.unit,
-      marginRight: theme.spacing.unit,
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
     },
     dense: {
-      marginTop: 16,
+        marginTop: 16,
     },
     menu: {
-      width: 200,
+        width: 200,
     },
-  });
+});
 const TodoForm = () => {
+    
     return (
-        <div>
-
-        </div>
+        <>
+            <TextField
+                id="outlined-name"
+                label="Name"
+                className={classes.textField}
+                value={this.state.name}
+                onChange={this.handleChange('name')}
+                margin="normal"
+                variant="outlined"
+            />
+        </>
     )
 }
 
