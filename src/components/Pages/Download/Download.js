862952d553eece1ef91946db8a749761b33d7dbd
Prabeen04 <prabeen.strange@gmail.com>
2018-11-23 17:50:52 +0530

    small stuff

--- src/components/Pages/Download/Download.js
+++ src/components/Pages/Download/Download.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 const Download = () => {
     useEffect(() => {
-        fetch(`http://192.168.1.61:8000/employeeList`)
+        fetch(`http://192.168.1.61:8000/exportEmployeeExcel`)
             .then(res => res.json())
             .then(res => {
                 console.log(res)
@@ -13,6 +13,7 @@ const Download = () => {
     return (
         <div>
             <h1>Download Component</h1>
+            <a href="http://192.168.1.61:8000/exportEmployeeExcel">download</a>
         </div>
     )
 }
