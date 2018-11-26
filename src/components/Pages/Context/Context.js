f7a6d6c8287a4fe3c1f092b3fd60dbdadfd4db13
Prabeen04 <prabeen.strange@gmail.com>
2018-11-26 17:24:03 +0530

    context consumer created

--- src/components/Pages/Context/Context.js
+++ src/components/Pages/Context/Context.js
@@ -3,9 +3,12 @@ import React, { createContext } from 'react'
 const TimeContext = createContext(Date.now())
 
 const Context = (props) => {
-  return (
-    <>
-        <TimeContext.Provider value={time: 'now'}>      
+    return (
+        <>
+            <TimeContext.Provider value={{time: 'now'}}>
+            <TimeContext.Consumer>
+                {(props) => console.log(props)}
+            </TimeContext.Consumer>
             <h3>Context Component</h3>
         </TimeContext.Provider>
     </>
