029c2cf87f9f922a03485b832fef2d9e6dc8b26d
Prabeen04 <prabeen.strange@gmail.com>
2018-11-30 22:00:48 +0530

    consumer added in context

--- src/components/Pages/Context/Context.js
+++ src/components/Pages/Context/Context.js
@@ -1,18 +1,21 @@
 import React, { createContext } from 'react'
 import moment from 'moment';
+
+import Consumer from './Consumer';
 const TimeContext = createContext(moment())
 
 const Context = (props) => {
     return (
         <>
-            <TimeContext.Provider value={{time: moment().toLocaleString()}}>
-            <TimeContext.Consumer>
-                {(props) => <pre>{props.time}</pre>}
-            </TimeContext.Consumer>
-            <h3>Context Component</h3>
-        </TimeContext.Provider>
-    </>
-  )
+            <TimeContext.Provider value={{ time: moment().toLocaleString() }}>
+                <TimeContext.Consumer>
+                    {(props) => <pre>{props.time}</pre>}
+                </TimeContext.Consumer>
+                <h3>Context Component</h3>
+                <Consumer />
+            </TimeContext.Provider>
+        </>
+    )
 }
 
 export default Context;
