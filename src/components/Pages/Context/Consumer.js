b39f0b629e393b8fda88df9b3199ede822c60810
Prabeen04 <prabeen.strange@gmail.com>
2018-11-30 22:07:02 +0530

    static context type added

--- src/components/Pages/Context/Consumer.js
+++ src/components/Pages/Context/Consumer.js
@@ -1,14 +1,14 @@
 import React, { Context } from 'react'
-const TimeContext =  Context('TimeContext')
 const Consumer = (props) => {
-    return (
-        <div>
-            <h1>Consumer component</h1>
-            <TimeContext.Consumer>
-                {(props) => <pre>{props.time}</pre>}
-            </TimeContext.Consumer>
-        </div>
-    )
+    static contextType = TimeContext;
+return (
+    <div>
+        <h1>Consumer component</h1>
+        <TimeContext.Consumer>
+            {(props) => <pre>{props.time}</pre>}
+        </TimeContext.Consumer>
+    </div>
+)
 }
 
 export default Consumer;
