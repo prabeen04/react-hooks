654c1c25654ef751e657b3cea743ade2bf95a412
Prabeen04 <prabeen.strange@gmail.com>
2018-12-01 19:38:49 +0530

    context value retrived

--- src/components/Pages/Context/Consumer.js
+++ src/components/Pages/Context/Consumer.js
@@ -1,12 +1,11 @@
 import React, { useContext } from 'react'
 import { TimeContext } from "./TimeContext";
 const Consumer = (props) => {
+    const timeContext = useContext(TimeContext);
+    console.log(timeContext)
     return (
         <div>
             <h1>Consumer component</h1>
-            <TimeContext.Consumer>
-                {(props) => <pre>{props.time}</pre>}
-            </TimeContext.Consumer>
         </div>
     )
 }
