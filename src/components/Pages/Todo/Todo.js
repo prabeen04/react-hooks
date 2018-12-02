315741cc4a6ba65eded99c7ef62d0cf6d0df2341
Prabeen04 <prabeen.strange@gmail.com>
2018-12-02 15:37:01 +0530

    timeContext imported in todo

--- src/components/Pages/Todo/Todo.js
+++ src/components/Pages/Todo/Todo.js
@@ -1,10 +1,13 @@
-import React, { lazy, Suspense, useState } from 'react'
+import React, { lazy, Suspense, useState, useContext } from 'react'
+import { TimeContext } from "../Context/TimeContext";
 
 const TodoList = lazy(() => import('./TodoList'));
 const TodoForm = lazy(() => import('./TodoForm'));
 
 const Todo = () => {
   const [todos, setsTodos] = useState([{ todo: 'code', iscompleted: false }, { todo: 'code again', isCompleted: false }])
+  const timeContext = useContext(TimeContext);
+  
   const toggleTodo = (todo, index) => {
     const newTodos = [...todos]
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
