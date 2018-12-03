bfd49f26e5cde890ee867cddec02d93aa366717f
Prabeen04 <prabeen.strange@gmail.com>
2018-12-03 19:48:17 +0530

    context time value added to useState

--- src/components/Pages/Context/Context.js
+++ src/components/Pages/Context/Context.js
@@ -1,12 +1,13 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 import moment from 'moment';
 import { TimeContext } from './TimeContext';
 import Consumer from './Consumer';
 
 const Context = (props) => {
+    const [time, setTime ] = useState({ time: moment().toLocaleString() })
     return (
         <>
-            <TimeContext.Provider value={{ time: moment().toLocaleString() }}>
+            <TimeContext.Provider value={time}>
                 <h3>Context Component</h3>
                 <Consumer />
             </TimeContext.Provider>
