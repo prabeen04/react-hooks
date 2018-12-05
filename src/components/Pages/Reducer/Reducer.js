81bcce78a1acde7e244d57c7c74346ff5fde6a8e
Prabeen04 <prabeen.strange@gmail.com>
2018-12-05 19:51:55 +0530

    time dispalyed from reducer

--- src/components/Pages/Reducer/Reducer.js
+++ src/components/Pages/Reducer/Reducer.js
@@ -1,13 +1,17 @@
 import React, { useReducer } from 'react'
 import moment from 'moment';
+import Typography from '@material-ui/core/Typography'
 const appReducer = (state, action) => {
   return state;
 }
 const Reducer = () => {
-  const [state, dispatch] = useReducer(appReducer, { time: moment()})
+  const [state, dispatch] = useReducer(appReducer, { time: moment() })
   return (
     <>
       <h2>Reducer component</h2>
+      <Typography variant='display1'>
+        {moment(state.time).toString()}
+      </Typography>
     </>
   )
 }
