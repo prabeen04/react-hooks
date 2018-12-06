8bdb446d487e224862c4e405183117d43997287d
Prabeen04 <prabeen.strange@gmail.com>
2018-12-06 15:23:52 +0530

    icon styles and Icon added

--- src/components/Pages/Reducer/TimeButtons.js
+++ src/components/Pages/Reducer/TimeButtons.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import DeleteIcon from '@material-ui/icons/Delete';
+import NavigationIcon from '@material-ui/icons/Navigation'
 
 const styles = theme => ({
     button: {
@@ -10,13 +13,22 @@ const styles = theme => ({
     input: {
         display: 'none',
     },
+    leftIcon: {
+        marginRight: theme.spacing.unit,
+    },
+    rightIcon: {
+        marginLeft: theme.spacing.unit,
+    }
 });
 
 function TimeButton(props) {
     const { classes } = props;
     return (
         <>
-            <Button variant="contained" color="primary" className={classes.button}>Increase Time</Button>
+            <Button variant="contained" color="primary" className={classes.button}>
+                <DeleteIcon className={classes.rightIcon} />
+                Increase Time
+            </Button>
             <Button variant="contained" color="secondary" className={classes.button}>Decrease Time</Button>
         </>
     );
