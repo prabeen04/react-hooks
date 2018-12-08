effe448f78cf7822f83a7295c48e3263bf357018
Prabeen04 <prabeen.strange@gmail.com>
2018-12-08 14:20:21 +0530

    flex container

--- src/components/Pages/Reducer/TimeButtons.js
+++ src/components/Pages/Reducer/TimeButtons.js
@@ -6,7 +6,7 @@ import Icon from '@material-ui/core/Icon';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 import NavigationIcon from '@material-ui/icons/Navigation'
-
+import { FlexContainer } from "../../UI/Layouts";
 const styles = theme => ({
     button: {
         margin: theme.spacing.unit,
@@ -26,15 +26,18 @@ function TimeButton(props) {
     const { classes, dispatch, time, viewType } = props;
     return (
         <>
-            <Button variant="contained" color="primary" className={classes.button}
-                onClick={() => dispatch({ type: 'INCREASE_TIME', payload: time })}>
-                <AddIcon className={classes.leftIcon} />{`Increase ${viewType}`}
-            </Button>
-            <Button variant="contained" color="secondary" className={classes.button}
-                onClick={() => dispatch({ type: 'DECREASE_TIME', payload: time })}>
-                <RemoveIcon className={classes.leftIcon} />
-                {`Decrease ${viewType}`}
-            </Button>
+            <FlexContainer>
+                <Button variant="contained" color="primary" className={classes.button}
+                    onClick={() => dispatch({ type: 'INCREASE_TIME', payload: time })}>
+                    <AddIcon className={classes.leftIcon} />{`Increase ${viewType}`}
+                </Button>
+                <Button variant="contained" color="secondary" className={classes.button}
+                    onClick={() => dispatch({ type: 'DECREASE_TIME', payload: time })}>
+                    <RemoveIcon className={classes.leftIcon} />
+                    {`Decrease ${viewType}`}
+                </Button>
+            </FlexContainer>
+            <FlexContainer></FlexContainer>
         </>
     );
 }
