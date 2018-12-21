d307ea9590fe9d6b504bd9f7583386080dc4aa89
Prabeen04 <prabeen.strange@gmail.com>
2018-12-21 18:42:30 +0530

    map added

--- src/components/Pages/Map/Map.js
+++ src/components/Pages/Map/Map.js
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 const Map = () => {
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <Map 
+            center={position}  
+           >
+                <TileLayer
+                    attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <MyMarkersList markers={markers} />
+            </Map>
+        </>
+    )
 }
 
 export default Map;
