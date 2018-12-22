65b3c4d8b4c3f3fdd623a53fadccf67b5506ccaa
Prabeen04 <prabeen.strange@gmail.com>
2018-12-22 20:06:27 +0530

    Map component added

--- src/components/Pages/MyMap/MyMap.js
+++ src/components/Pages/MyMap/MyMap.js
@@ -8,7 +8,17 @@ const MyMap = (props) => {
     })
     return (
         <>
-            <h1>map</h1>
+            <Map center={position} zoom={this.state.zoom}>
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+                </Marker>
+            </Map>
 
         </>
     )
