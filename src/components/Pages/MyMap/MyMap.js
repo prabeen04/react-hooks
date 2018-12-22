9f3352253f6347c0d4cbf25a664e284e8d3910a7
Prabeen04 <prabeen.strange@gmail.com>
2018-12-22 20:03:40 +0530

    junk code removed

--- src/components/Pages/MyMap/MyMap.js
+++ src/components/Pages/MyMap/MyMap.js
@@ -1,18 +1,9 @@
 import React, { useState } from 'react'
-// import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 const MyMap = () => {
     return (
         <>
         <h1>map</h1>
-            {/* <Map 
-            center={position}  
-           >
-                <TileLayer
-                    attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <MyMarkersList markers={markers} />
-            </Map> */}
+
         </>
     )
 }
