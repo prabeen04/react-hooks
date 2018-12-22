2d58686aa557f3e8277efbee312b8937083fab15
Prabeen04 <prabeen.strange@gmail.com>
2018-12-22 19:56:28 +0530

    small stuff

--- src/components/Pages/MyMap/MyMap.js
+++ src/components/Pages/MyMap/MyMap.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+// import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 const MyMap = () => {
     return (
         <>
-            <Map 
-            // center={position}  
+        <h1>map</h1>
+            {/* <Map 
+            center={position}  
            >
                 <TileLayer
                     attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {/* <MyMarkersList markers={markers} /> */}
-            </Map>
+                <MyMarkersList markers={markers} />
+            </Map> */}
         </>
     )
 }
