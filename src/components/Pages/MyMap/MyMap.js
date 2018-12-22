77a104efc4a3c64fb6c01acb484680f3c710cb65
Prabeen04 <prabeen.strange@gmail.com>
2018-12-22 20:07:42 +0530

    leaflet props passed

--- src/components/Pages/MyMap/MyMap.js
+++ src/components/Pages/MyMap/MyMap.js
@@ -6,9 +6,10 @@ const MyMap = (props) => {
         lng: -0.09,
         zoom: 13,
     })
+    const position = [data.lat, data.lng]
     return (
         <>
-            <Map center={position} zoom={this.state.zoom}>
+            <Map center={position} zoom={data.zoom}>
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
