849560088a81a208cf40d73a24b8d30b951aad4a
Prabeen04 <prabeen.strange@gmail.com>
2018-12-22 20:05:42 +0530

    state data defined

--- src/components/Pages/MyMap/MyMap.js
+++ src/components/Pages/MyMap/MyMap.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
-const MyMap = () => {
+import React, { useState } from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+const MyMap = (props) => {
+    const [data, setData] = useState({
+        lat: 51.505,
+        lng: -0.09,
+        zoom: 13,
+    })
     return (
         <>
-        <h1>map</h1>
+            <h1>map</h1>
 
         </>
     )
