9d116941466deb8cba0694ed9ad064619e33059a
Prabeen04 <prabeen.strange@gmail.com>
2018-12-23 10:10:20 +0530

    small stuff

--- src/components/Pages/MyMap/MapContainer.js
+++ src/components/Pages/MyMap/MapContainer.js
@@ -5,7 +5,7 @@ const MapContainer = (props) => {
     const position = [props.data.lat, props.data.lng]
     return (
         <>
-            <Map center={position} zoom={props.data.zoom} style={{width: '100vw', height: 400, margin: 10}}>
+            <Map center={position} zoom={props.data.zoom} style={{width: '100vw', height: '85vh', margin: 10}}>
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
