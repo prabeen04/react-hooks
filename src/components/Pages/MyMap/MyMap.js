1ae8bafed96fcb1c8df955fda47a9916aea88a3c
Prabeen04 <prabeen.strange@gmail.com>
2018-12-24 13:14:13 +0530

    Multiplemarker added

--- src/components/Pages/MyMap/MyMap.js
+++ src/components/Pages/MyMap/MyMap.js
@@ -1,5 +1,14 @@
 import React, { useState, lazy, Suspense } from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+const markers = [
+    { key: 'marker1', position: [11.5, 10.1], children: 'My first popup' },
+    { key: 'marker2', position: [51.51, -0.1], children: 'My second popup' },
+    { key: 'marker3', position: [2.49, 20.05], children: 'My third popup' },
+    { key: 'marker4', position: [52.49, 20.05], children: 'My fourth popup' },
+    { key: 'marker5', position: [29.49, 20.05], children: 'My fifth popup' },
+    { key: 'marker6', position: [2.49, 9.05], children: 'My sixth popup' },
+]
 const MapWrapper = styled.div`
     margin: 0 auto;
 `
