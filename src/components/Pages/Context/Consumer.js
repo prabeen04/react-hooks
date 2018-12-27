70e866b8a829a6f24c4555069192ce3c0a0d3d1e
Prabeen04 <prabeen.strange@gmail.com>
2018-12-27 09:03:28 +0530

    conversion to time from full date

--- src/components/Pages/Context/Consumer.js
+++ src/components/Pages/Context/Consumer.js
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
+import moment from "moment";
 import Typography from "@material-ui/core/Typography";
 import { TimeContext } from "./TimeContext";
 const Consumer = (props) => {
     const timeContext = useContext(TimeContext);
     return (
         <div>
-            <Typography variant='headline'>{timeContext.time}</Typography>
+            <Typography variant='headline'>{moment(timeContext.time, 'HH:mm:ss').toString()}</Typography>
         </div>
     )
 }
