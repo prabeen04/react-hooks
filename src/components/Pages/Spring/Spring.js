e4e432a4c8a923f1632ebd9d4e76dc0fb222ec76
Prabeen04 <prabeen.strange@gmail.com>
2018-12-28 21:04:06 +0530

    icons removed

--- src/components/Pages/Spring/Spring.js
+++ src/components/Pages/Spring/Spring.js
@@ -2,21 +2,21 @@ import React, { useState, useRef } from 'react';
 import { useSpring, animated } from 'react-spring/hooks';
 const cards = [
     {
-      title: 'Build faster ⚡️',
+      title: 'Build faster',
       description:
         'Create a React web app in the fraction of the time using our library of themes and building blocks. We have everything from navbars and content grids to authentication flows and commenting systems. New blocks are added every week.',
       image: 'https://divjoy.com/static/images/undraw_collection.svg',
       imageRatio: 784 / 1016
     },
     {
-      title: 'Tweak anything 👩‍🎨',
+      title: 'Tweak anything',
       description:
         'Built with developers in mind. Change element structure, edit CSS, create components, add props and state. We give you access to the underlying React code so you can do what you need right in our tool.',
       image: 'https://divjoy.com/static/images/undraw_upload.svg',
       imageRatio: 839 / 1133
     },
     {
-      title: 'Export your code 🚀',
+      title: 'Export your code',
       description:
         "Export your project as a high-quality React codebase. We're lazer focused on helping you build and iterate quickly, but expect that you'll eventually want to export and wrap things up in your favorite code editor.",
       image: 'https://divjoy.com/static/images/undraw_static_assets.svg',
@@ -29,13 +29,13 @@ function Spring() {
   return (
     <div className="container">
       <div className="row">
-        {cards.map((card, i) => (
+        {Array.from(cards).map((card, i) => (
           <div className="column">
-            {/* <Card> */}
+            <Card>
               <div className="card-title">{card.title}</div>
               <div className="card-body">{card.description}</div>
               <img className="card-image" src={card.image} />
-            {/* </Card> */}
+            </Card>
           </div>
         ))}
       </div>
