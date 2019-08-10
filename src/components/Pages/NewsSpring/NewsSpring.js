69f6508a057dbd2ea9b8d14d61252f4dbabc5c77
Prabeen04 <prabeen.strange@gmail.com>
2019-08-10 20:08:44 +0530

    small stuff

--- src/components/Pages/NewsSpring/NewsSpring.js
+++ src/components/Pages/NewsSpring/NewsSpring.js
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from 'react'
 import { useTransition, useSpring, useChain, config } from 'react-spring'
-import { Global, Container, Item } from './style'
-// import data from './data'
+import { Container, Item } from './style'
 
 export default function NewsSpring({ data, selectedSource, setSelectedSource }) {
   const [open, set] = useState(false)
@@ -9,7 +8,7 @@ export default function NewsSpring({ data, selectedSource, setSelectedSource }) 
   const springRef = useRef()
   const { size, opacity, ...rest } = useSpring({
     ref: springRef,
-    config: config.stiff,
+    // config: config.stiff,
     from: { size: '20%', background: 'hotpink' },
     to: { size: open ? '100%' : '20%', background: open ? 'white' : 'hotpink' }
   })
@@ -25,24 +24,19 @@ export default function NewsSpring({ data, selectedSource, setSelectedSource }) 
   })
 
   // This will orchestrate the two animations above, comment the last arg and it creates a sequence
-  useChain(open ? [springRef, transRef] : [transRef, springRef], [0, open ? 0.1 : 0.6])
-  function handleContainerClick(a, b, c) {
-    console.log('container clicked')
+  useChain(open ? [springRef, transRef] : [transRef, springRef], [0, open ? 0.1 : 0.0002])
+  function handleContainerClick() {
     set(!open)
-    console.log(a, b, c)
   }
   function handleItemClick(e, item) {
-    console.log('item clicked')
     e.stopPropagation()
     set(!open)
     setSelectedSource(item.id)
-    console.log(e, item)
   }
   return (
     <>
-      {/* <Global /> */}
       <Container style={{ ...rest, width: size, height: size, position: 'relative' }} onClick={handleContainerClick}>
-        <h3 style={{position: 'absolute', left: '30%', top: '30%', color: '#fff', fontWeight: 'lighter', letterSpacing: '3px'}}>Select News</h3>
+        <h3 style={{position: 'absolute', left: '30%', top: '30%', color: '#fff', fontWeight: 'normal', letterSpacing: '3px'}}>Select News</h3>
         {transitions.map(({ item, key, props }) => (
           <Item key={key} style={{ ...props, background: item.css }} onClick={(e) => handleItemClick(e, item)}>
             {item.name}
