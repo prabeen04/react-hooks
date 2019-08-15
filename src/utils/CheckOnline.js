ce116d225409c91ef05ef3c2c70c0bdf416091bb
Prabeen04 <prabeen.strange@gmail.com>
2019-08-15 21:51:13 +0530

    CheckOffile component created

---
+++ src/utils/CheckOnline.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CheckOnline() {
+    return (
+        <div>
+            <h1>You are offline, connect to internet to get access .</h1>
+        </div>
+    )
+}
