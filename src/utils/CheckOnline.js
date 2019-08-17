4b49f9678ae79b1c341da5148b55415036640f9b
Prabeen04 <prabeen.strange@gmail.com>
2019-08-17 20:03:02 +0530

    CheckOnline component styles

--- src/utils/CheckOnline.js
+++ src/utils/CheckOnline.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import styled from 'styled-components';
 
 export default function CheckOnline() {
     const isOnline = window.navigator.onLine;
-    if(!isOnline) return null;
+    if (!isOnline) return null;
     return (
-        <div>
-            <h1>You are offline, connect to internet to get access .</h1>
-        </div>
+        <CheckOnlineContainer>
+            <MessageText>You are offline, connect to internet to get access .</MessageText>
+        </CheckOnlineContainer>
     )
 }
+
+const CheckOnlineContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5)
+    display: flex;
+    justify-content: center;
+    align-item: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+`;
+
+const MessageText = styled.h1`
+    background-color: teal;
+    color: #fff;
+    font-weight: 600;
+`
