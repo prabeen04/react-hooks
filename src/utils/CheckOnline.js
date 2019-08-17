bbd60469aaa1fc09aabcdb721956aa705f86a036
Prabeen04 <prabeen.strange@gmail.com>
2019-08-17 20:35:01 +0530

    offline message container styles

--- src/utils/CheckOnline.js
+++ src/utils/CheckOnline.js
@@ -16,7 +16,7 @@ export default function CheckOnline() {
             window.removeEventListener('online', handleConnectionChange)
         }
     }, [])
-    if (isOnline) return null;
+    if (!isOnline) return null;
     return (
         <CheckOnlineContainer>
             <MessageText>You are offline, connect to internet to get access .</MessageText>
@@ -25,20 +25,26 @@ export default function CheckOnline() {
 }
 
 const CheckOnlineContainer = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-color: rgba(0, 0, 0, 0.5)
-    display: flex;
-    justify-content: center;
-    align-item: center;
     position: fixed;
+    z-index: 100000;
     top: 0;
     left: 0;
-    z-index: 9999;
+    min-height: 100vh;
+    min-width: 100vw;
+    background: rgba(1, 8, 46, 0.45);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+
 `;
 
 const MessageText = styled.h1`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 200px;
     background-color: teal;
     color: #fff;
-    font-weight: 600;
+    padding: 1rem 2rem;
 `
